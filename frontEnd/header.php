8e870ba41e21fc089622b7a573d6288674391832
Terki Adel <terki.adel@yahoo.com>
2021-03-25 00:39:27 +0100

    working on a better header

--- frontEnd/header.php
+++ frontEnd/header.php
@@ -21,114 +21,24 @@ ob_end_clean();?>
 <div class="header-container">
     <header class="app-header" :class="{'search-active': showSearch, 'navigation-active': showNavigation}"
         id="app-header">
-        <div class="main-bar desktop-bar">
-            <div class="container">
-                <div class="header-left">
-                    <div class="header-element">
-                        <a class="header-brand" href="/">
-                            <img src="<?php echo $ROOT_DIR . '/assets/theme-settings/pf logo.png' ?>"
-                                alt="PolimaxFrance™" />
-                        </a>
-                    </div>
-                </div>
-                <div class="header-center">
-                    <div class="header-element">
-                        <ul class="list-unstyled header-list">
-                            <li>
-                                <a href="/pages/paiement.php" >
-                                    Commander et payer 
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/a-propos-de-la-livraison.php" >
-                                    Livraison 
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/about-us.php" >
-                                    À propos de nous 
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/pages/contact-us.php" >
-                                    Contactez-nous 
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="header-right">
-                </div>
-            </div>
-        </div>
-        <div class="main-bar mobile-bar">
-            <div class="container">
-                <div class="header-center">
-                    <div class="header-element">
-                        <a class="header-brand" href="/">
-                            <img src="<?php echo $ROOT_DIR . '/assets/theme-settings/pf logo.png' ?>"
-                                alt="PolimaxFrance™" />
-                        </a>
-                    </div>
-                </div>
-                <div class="header-right">
-                    <div class="header-element">
-                        <a class="header-switcher" href="javascript:void(0)" role="button" onclick="openNav()">
-                            <i class="fa fa-navicon" ></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="overlay" onclick="closeNav()"></div>
-        <nav class="side-navigation" id="mySidebar" >
-            <div> 
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <a class="navigation-brand" href="/">
-                    <img src="<?php echo $ROOT_DIR . '/assets/theme-settings/pf logo.png' ?>" alt="PolimaxFrance™" />
-                </a>
-            </div>
-            <form class="navigation-search">
-                <div class="search-input">
-                    <input type="search" placeholder="Rechercher un produit / أبحث عن" required name="query">
-                    <button type="submit">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </form>
-            <ul class="list-unstyled navigation-list">
-                <li>
-                    <a href="/pages/paiement.php">Comment commander et payer ? </a>
-                </li>
-                <li>
-                    <a href="/pages/a-propos-de-la-livraison.php">À Propos De La Livraison</a>
-                </li>
-                <li>
-                    <a href="/pages/about-us.php">À Propos De Nous</a>
-                </li>
-                <li>
-                    <a href="/pages/contact-us.php">Contactez-nous</a>
-                </li>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container-fluid">
+            <a class="navbar-brand header-brand" href="/">
+                <img src="<?php echo $ROOT_DIR . '/assets/theme-settings/pf logo.png' ?>"
+                                    alt="PolimaxFrance™" />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav list-unstyled header-list">
+                <li><a class="nav-link" href="/pages/paiement.php">Commander et payer </a></li>
+                <li><a class="nav-link" href="/pages/a-propos-de-la-livraison.php">Livraison</a></li>
+                <li><a class="nav-link" href="/pages/about-us.php">À propos de nous</a></li>
+                <li><a class="nav-link" href="/pages/contact-us.php">Contactez-nous</a></li>
             </ul>
-        </nav>
-        <form class="search-form" method="GET">
-            <div class="container">
-                <div class="search-select">
-                    <select title="collections" name="collection">
-                        <option value="" selected>Toutes les Collections</option>
-                        <option value="28b040aa-f760-4a85-8fb2-b4067aa47871">Literie de luxe</option>
-                        <option value="31213732-09dc-4071-b5cf-3696af8efe94">Le Premier Classement</option>
-                        <option value="528fa9a7-f93d-4505-a32b-43811b08063c">Le Deuxiéme Classement</option>
-                    </select>
-                </div>
-                <div class="search-input">
-                    <input type="hidden" name="limit" value="12">
-                    <input type="search" placeholder="Rechercher un produit" required name="q" value="">
-                    <button class="search-submit" type="submit">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
             </div>
-        </form>
+        </div>
+        </nav>
     </header>
 </div>
--- frontEnd/products/couvre-lit-doubles-faces-4-taies-et-4-oreillers-ergonomiques-polimaxfrancegratuits.php
+++ frontEnd/products/couvre-lit-doubles-faces-4-taies-et-4-oreillers-ergonomiques-polimaxfrancegratuits.php
@@ -2,139 +2,79 @@
 <html dir="ltr" lang="fr_FR">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="theme-color" content="#FFF" />
-    <meta name="msapplication-navbutton-color" content="#FFF" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-    <meta name="format-detection" content="telephone=no" />
-    <meta name="csrf-token" content="QoP4XWdKk9b4fHDVajmdcMvymEqo4RrKuaHrJaaq">
-    <title>PolimaxFrance™</title>
-    <meta name="description"
-        content="PolimaxFrance&trade; est une entreprise sp&eacute;cialis&eacute; dans la literie de luxe a prix comp&eacute;titive">
-    <meta property="og:title" content="PolimaxFrance™" />
-    <meta property="og:description"
-        content="PolimaxFrance&trade; est une entreprise sp&eacute;cialis&eacute; dans la literie de luxe a prix comp&eacute;titive" />
-    <meta property="og:image" content="../assets/theme-settings/pf logo.png" />
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta name="theme-color" content="#FFF" />
+  <meta name="msapplication-navbutton-color" content="#FFF" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+  <meta name="format-detection" content="telephone=no" />
+  <meta name="csrf-token" content="QoP4XWdKk9b4fHDVajmdcMvymEqo4RrKuaHrJaaq" />
+  <title>PolimaxFrance™</title>
+  <meta name="description"
+    content="PolimaxFrance&trade; est une entreprise sp&eacute;cialis&eacute; dans la literie de luxe a prix comp&eacute;titive" />
+  <meta property="og:title" content="PolimaxFrance™" />
+  <meta property="og:description"
+    content="PolimaxFrance&trade; est une entreprise sp&eacute;cialis&eacute; dans la literie de luxe a prix comp&eacute;titive" />
+  <meta property="og:image" content="../assets/theme-settings/pf logo.png" />
 
-    <meta name="twitter:title" content="PolimaxFrance™" />
-    <meta name="twitter:description"
-        content="PolimaxFrance&trade; est une entreprise sp&eacute;cialis&eacute; dans la literie de luxe a prix comp&eacute;titive" />
-    <meta name="twitter:images0" content="../assets/theme-settings/pf logo.png" />
-    <link rel="icon" type="image/png" href="../assets/others/j2FOwzDTShs5QtrfRufzGTkbSpkaAG9b6Njkk2qS.png" />
-    <link href="../css/header.css" rel="stylesheet">
-    <link href="../css/footer.css" rel="stylesheet">
-    <link href="../css/app.css" rel="stylesheet"> 
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    <link href="../css/navigation.css" rel="stylesheet"> 
-    <!-- Custom CSS Code -->
-    <script>
-    ! function(f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function() {
-            n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = '2.0';
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
-    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '789700358549345');
-    fbq('track', 'PageView');
-    </script>
-
-
-
-    <style type="text/css">
-    .section-xsLzTqivSfGLMi9o {
-        padding-top: 30px;
-        padding-bottom: 30px;
-        background-color: #ffffff;
-        color: #000000;
-    }
-
-    .section-xsLzTqivSfGLMi9o .heading-primary {
-        color: #000000;
-    }
-
-    .section-xsLzTqivSfGLMi9o .heading-description {
-        color: #696969;
-    }
-
-    .section-xsLzTqivSfGLMi9o .product-item .product-title a {
-        color: #000000;
-    }
-
-    .section-xsLzTqivSfGLMi9o .product-item .product-price .before {
-        color: #747474;
-    }
-
-    .section-xsLzTqivSfGLMi9o .product-item .product-price .after {
-        color: #D0021B;
-    }
-
-    .section-xsLzTqivSfGLMi9o .product-item,
-    .section-xsLzTqivSfGLMi9o .product-item .product-details {
-        border-color: #f0f0f0;
-    }
-
-    .section-xsLzTqivSfGLMi9o .product-item:hover,
-    .section-xsLzTqivSfGLMi9o .product-item:hover .product-details {
-        border-color: #000000;
-    }
-
-    .section-xsLzTqivSfGLMi9o .product-item .product-actions .button {
-        color: #FFFFFF;
-        background-color: #2CBF40;
-        border-color: #2CBF40;
-    }
-
-    .section-xsLzTqivSfGLMi9o .product-item .product-actions .button:hover {
-        color: #000000;
-        background-color: #11D52C;
-        border-color: #11D52C;
-    }
-
-    .section-xsLzTqivSfGLMi9o .load-more a,
-    .section-xsLzTqivSfGLMi9o .products-slider .slick-next,
-    .section-xsLzTqivSfGLMi9o .products-slider .slick-prev {
-        color: #696969;
-        background-color: #ffffff;
-        border-color: #f0f0f0;
-    }
-
-    .section-xsLzTqivSfGLMi9o .load-more a:hover,
-    .section-xsLzTqivSfGLMi9o .products-slider .slick-next:hover,
-    .section-xsLzTqivSfGLMi9o .products-slider .slick-prev:hover {
-        color: #000000;
-        background-color: #ffffff;
-        border-color: #000000;
-    }
-    </style>
+  <meta name="twitter:title" content="PolimaxFrance™" />
+  <meta name="twitter:description"
+    content="PolimaxFrance&trade; est une entreprise sp&eacute;cialis&eacute; dans la literie de luxe a prix comp&eacute;titive" />
+  <meta name="twitter:images0" content="../assets/theme-settings/pf logo.png" />
+  <link rel="icon" type="image/png" href="../assets/others/j2FOwzDTShs5QtrfRufzGTkbSpkaAG9b6Njkk2qS.png" />
+  <link href="../css/header.css" rel="stylesheet" />
+  <link href="../css/footer.css" rel="stylesheet" />
+  <link href="../css/app.css" rel="stylesheet" />
+  <link href="../css/products/style.css" rel="stylesheet" />
+  <!--     <link href="../css/index.css" rel="stylesheet" /> -->
+  <link href="../css/bootstrap.css" rel="stylesheet" />
+  <link href="../css/navigation.css" rel="stylesheet" />
+  <!-- Custom CSS Code -->
+  <script>
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(
+      window,
+      document,
+      "script",
+      "https://connect.facebook.net/en_US/fbevents.js"
+    );
+    fbq("init", "789700358549345");
+    fbq("track", "PageView");
+  </script>
 </head>
 
 <body>
-    <div id="app">
-        <!-- App header -->
-        <div id="header-wrapper" class="header-wrapper notice-down-mobile">
-            <?php include "../header.php" ?>
-        </div>
-        <!-- Toast messages -->
-        <flash></flash>
-        <main class="page-wrapper">
-            <h1 itemprop="name" class="product-single__title text-center">
-                Couvre-lit-doubles-faces-4-taies-et-4-oreillers-ergonomiques-polimaxfrancegratuits</h1>
-            <div class="container">
-                <div class="col-md-12">
+  <div id="app">
+    <!-- App header -->
+    <div id="header-wrapper" class="header-wrapper notice-down-mobile">
+      <?php include "../header.php" ?>
+    </div>
+    <!-- Toast messages -->
+    <flash></flash>
+    <main class="page-wrapper">
+      <h5 itemprop="name" class="product-single__title text-center">
+        Couvre-lit-doubles-faces-4-taies-et-4-oreillers-ergonomiques-polimaxfrancegratuits
+      </h5>
+      <!--<div class="container">
+               <div class="col-md-12">
                     <div class="product col-md-3 service-image-left">
 
                         <center>
@@ -146,7 +86,7 @@
                 </div>
 
                 <div>
-                    <div class="product-desc">
+                    <div class="product-info">
                         Couvre-lit-doubles-faces-4-taies-et-4-oreillers-ergonomiques-polimaxfrancegratuits</div>
                     <div class="product-price">
                         <span class="currency-value before">
@@ -168,21 +108,119 @@
                         </button>
                     </div>
                 </div>
+ 
+                </div>-->
+      <div class="container mt-5 mb-5">
+        <div class="row d-flex justify-content-center">
+          <div class="col-md-10">
+            <div class="product-quantity">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="images p-3">
+                    <div class="text-center p-4">
+                      <img id="main-image" src="../assets/products/T4U5J59II3YeuDEKWjyIeHOEJ1Pq6HE1emDZVoHI_md.png"
+                        width="300" />
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="product p-4">
+                    <div class="mt-4 mb-3">
+                      <div class="prices">
+                        <span class="price-value before">
+                          <span class="value">19999</span><span class="currency">&nbsp;DZD</span>
+                        </span>
+                        <span class="price-value after">
+                          <span class="value">9999</span><span class="currency">&nbsp;DZD</span>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="checkout-section">
+                      <h6>Dimensions et quantité</h6></br>
+                      <ol class="radio-group">
+                        <label>
+                          <input type="radio" class="radio" name="size" value="S" checked />
+                          <span>Couvre lit double faces 100cm*190cm + 4 taies + 4 oreillers -> 9900 DZD</span>
+                        </label>
+                        </br>
+                        <label>
+                          <input type="radio" class="radio" name="size" value="M" />
+                          <span>Couvre lit double faces 120cm*190cm + 4 taies + 4 oreillers -> 12000 DZD</span>
+                        </label>
+                        </br>
+                        <label>
+                          <input type="radio" class="radio" name="size" value="L" />
+                          <span>Couvre lit double faces 140cm*200cm + 4 taies + 4 oreillers -> 16000 DZD</span>
+                        </label>
+                        </br>
+                        <label>
+                          <input type="radio" class="radio" name="size" value="XL">
+                          <span>Couvre lit double faces 160cm*200cm + 4 taies + 4 oreillers -> 18000 DZD</span>
+                        </label>
+                        </br>
+                        <label>
+                          <input type="radio" class="radio" name="size" value="XXL" />
+                          <span>Couvre lit double faces 180cm*220cm + 4 taies + 4 oreillers -> 20000 DZD</span>
+                        </label>
+                        </br>
+                      </ol>
+                    </div>
+                    <div class="auth-section">
+                      <h6>Informations client</h6></br>
+                      <form class="grid-form-group">
+                        <div class="form-group">
+                          <input id="firstName" type="text" name="firstName" placeholder="Prenom" value="" required>
+                        </div>
+                        <div class="form-group">
+                          <input id="lastName" type="text" name="lastName" placeholder="Nom " value="" required>
+                        </div>
+                        <div class="form-group">
+                          <input id="town" type="text" name="town" placeholder="Ville" value="" required>
+                        </div>
+                        <div class="form-group">
+                          <input id="tel" type="text" name="yel" placeholder="Tél " value="" required>
+                        </div>
+                        <div class="product-actions">
+                          <button class="button small-button secondary-button">
+                            RESERVER MAINTENANT
+                          </button>
+                          <!-- 
+                                    <element class="checkout-section">
+                                        …
+                                          <element class="checkout">
+                                            …
+                                              <element class="sidebar">
+                                                …
+                                                  <element class="aside-body">
+                                                    …
+                                                      <element class="aside-products">
+                                                        …
+                                                          <input>
+                                  -->
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </main>
-        <!-- App footer -->
-        <div id="footer">
-            <?php include "../footer.php" ?>
+          </div>
         </div>
-        <!-- Hookables -->
+      </div>
+    </main>
+    <!-- App footer -->
+    <div id="footer">
+      <?php include "../footer.php" ?>
     </div>
+    <!-- Hookables -->
+  </div>
 
-    <!--     <script src="./js/bootstrap.js"></script>
+  <!--     <script src="./js/bootstrap.js"></script>
     <script src="./js/app.js"></script> -->
-    <!-- <script src="./js/home/app.js"></script> -->
-    <!--  <script type="module">
+  <!-- <script src="./js/home/app.js"></script> -->
+  <!--  <script type="module">
 let sliderId = "section-w54PAhmLeqZ6IRsW",
-    direction = "horizontal",
+      direction = "horizontal",
     frequency = "5",
     customHeight = 400,
     slideImages = document.querySelectorAll(isMobileView() ? '.desktop-image' : '.mobile-image');
@@ -201,37 +239,28 @@ document.addEventListener("DOMContentLoaded", () => {
     $(`#${sliderId}`).removeClass('show-first-image');
 });
 </script> -->
-    <!-- Custom JavaScript code -->
+  <!-- Custom JavaScript code -->
 
+  <!-- Google Analytics code -->
 
-    <!-- Google Analytics code -->
-
-    <div id="sales-notifications" class="sales-notifcation hidden">
-        <div class="sale">
-            <a aria-label="close" id="notification-close" class="close">
-                <i class="fa fa-x-circle"></i>
-            </a>
-            <div class="sale-thumbnail">
-                <img id="sale-notif-img">
-            </div>
-            <div class="sale-body">
-                <div class="message" id="notif-message"></div>
-                <div class="name" id="notif-name"></div>
-                <div class="date" id="notif-date"></div>
-            </div>
-            <a id="sale-link" target="_blank" class="sale-link"></a>
-        </div>
+  <div id="sales-notifications" class="sales-notifcation hidden">
+    <div class="sale">
+      <a aria-label="close" id="notification-close" class="close">
+        <i class="fa fa-x-circle"></i>
+      </a>
+      <div class="sale-thumbnail">
+        <img id="sale-notif-img" />
+      </div>
+      <div class="sale-body">
+        <div class="message" id="notif-message"></div>
+        <div class="name" id="notif-name"></div>
+        <div class="date" id="notif-date"></div>
+      </div>
+      <a id="sale-link" target="_blank" class="sale-link"></a>
     </div>
-    <script src="../js/navigation.js"></script>
-    <script src="../js/bootstrap-beta.js"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
-        integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
-        integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous">
-    </script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
+  </div>
+  <script src="../js/navigation.js"></script>
+  <script src="../js/bootstrap-beta.js"></script>
 </body>
 
 </html>
