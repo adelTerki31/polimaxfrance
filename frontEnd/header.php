1be959c1cd1a0e74e86f5625e2301214760052f7
Terki Adel <terki.adel@yahoo.com>
2021-03-26 12:02:33 +0100

    update for the header

--- frontEnd/header.php
+++ frontEnd/header.php
@@ -26,6 +26,51 @@ ob_end_clean();?>
   <header class="app-header" :class="{'search-active': showSearch, 'navigation-active': showNavigation}"
     id="app-header">
     <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container-fluid">
+          <div class="header-left">
+            <div class="header-element">
+              <a class="header-brand" href="/">
+                <img src="<?php echo $ROOT_DIR . '/assets/theme-settings/pf logo.png' ?>" alt="PolimaxFrance™" />
+              </a>
+            </div>
+          </div>
+          <div class="header-right">
+            <div class="header-element">
+              <div class="header-switcher">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                  aria-label="Toggle navigation">
+
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav list-unstyled header-list">
+              <li>
+                <a class="nav-link" href="/pages/paiement.php">Commander et payer
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="/pages/a-propos-de-la-livraison.php">Livraison</a>
+              </li>
+              <li>
+                <a class="nav-link" href="/pages/about-us.php">À propos de nous</a>
+              </li>
+              <li>
+                <a class="nav-link" href="/pages/contact-us.php">Contactez-nous</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+</div>
+<!-- <div class="main-bar mobile-bar">
+  <div class="container">
     <div class="header-center">
       <div class="header-element">
         <nav class="navbar navbar-expand-lg navbar-light bg-white">
@@ -40,14 +85,14 @@ ob_end_clean();?>
             <div class="header-right">
               <div class="header-element">
                 <div class="header-switcher">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                  data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                  aria-label="Toggle navigation">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
 
-                <span class="navbar-toggler-icon"></span>
-                </button>
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
                 </div>
-                </div>
+              </div>
             </div>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <ul class="navbar-nav list-unstyled header-list">
@@ -71,6 +116,8 @@ ob_end_clean();?>
       </div>
     </div>
   </div>
+</div> -->
+</div>
 </div>
 </header>
 </div>
